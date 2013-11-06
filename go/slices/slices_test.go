ec5c3ae10bd133bb57ee5a960fd96c6d7aceb550
Maximilien Riehl <max@riehl.io>
2013-11-06 17:51:06 +0100

    fix stupid mistake

--- go/slices/slices_test.go
+++ go/slices/slices_test.go
@@ -42,7 +42,7 @@ func TestShouldAddSeveralItemsAndIncreaseCapacity(t *testing.T) {
 }
 
 func assertHasGrown(s []int, originalLength int, t *testing.T) {
-	if hasGrown := cap(s) >= len(s) && len(s) > 1; !hasGrown {
+	if hasGrown := cap(s) >= len(s) && len(s) > originalLength; !hasGrown {
 		t.Errorf("Slice has not grown as expected (len %d and cap %d but "+
 			"should hold 2 elements)", len(s), cap(s))
 	}
