46238d0eefae667ec7d1e59bfd06e6ee47676a7a
mriehl <maximilien.riehl@gmail.com>
2013-11-08 11:22:34 +0100

    refactoring: extract function

--- go/pic/pic.go
+++ go/pic/pic.go
@@ -4,6 +4,13 @@ import (
 	"code.google.com/p/go-tour/pic"
 )
 
+func bluescaleValue(x, y int) (value uint8) {
+	_x := uint8(x)
+	_y := uint8(y)
+	value = (_x + _y) / uint8(2)
+	return
+}
+
 func Pic(dx, dy int) (bluescale [][]uint8) {
 	bluescale = make([][]uint8, dy)
 
@@ -11,8 +18,7 @@ func Pic(dx, dy int) (bluescale [][]uint8) {
 		bluescale[dxSliceIndex] = make([]uint8, dx)
 
 		for pixelIndex := range bluescale[dxSliceIndex] {
-			bluescale[dxSliceIndex][pixelIndex] = ((uint8(dxSliceIndex) +
-				uint8(pixelIndex)) / uint8(2))
+			bluescale[dxSliceIndex][pixelIndex] = bluescaleValue(dxSliceIndex, pixelIndex)
 		}
 
 	}
