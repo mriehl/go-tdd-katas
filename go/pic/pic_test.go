267695c074a178cc99b801c970e25ac4cd261dd6
mriehl <maximilien.riehl@gmail.com>
2013-11-08 17:10:09 +0100

    replace magic values with function call, unittest the function

--- go/pic/pic_test.go
+++ go/pic/pic_test.go
@@ -6,6 +6,16 @@ import (
 	"testing"
 )
 
+func TestShouldGenerateBluescaleValues(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			expected := uint8((i + j) / 2)
+			actual := BluescaleValue(i, j)
+			assert.Equal(t, actual, expected)
+		}
+	}
+}
+
 func TestShouldReturnSliceOfLengthDy(t *testing.T) {
 	ySlice := Pic(2, 4)
 	assert.Equal(t, len(ySlice), 4)
@@ -18,12 +28,10 @@ func TestShouldReturnDySlicesOfLengthDx(t *testing.T) {
 	}
 }
 
-/*
- *	Should use (x + y) / 2 as bluescale values
- */
-func TestShouldFillWithInterestingValues(t *testing.T) {
-	bluescales := Pic(4, 4)
+func TestShouldFillWithBluescaleValues(t *testing.T) {
+	bluescales := Pic(2, 2)
 	assert.Equal(t, bluescales,
-		[][]uint8{[]uint8{0x0, 0x0, 0x1, 0x1}, []uint8{0x0, 0x1, 0x1, 0x2},
-			[]uint8{0x1, 0x1, 0x2, 0x2}, []uint8{0x1, 0x2, 0x2, 0x3}})
+		[][]uint8{
+			[]uint8{BluescaleValue(0, 0), BluescaleValue(0, 1)},
+			[]uint8{BluescaleValue(1, 0), BluescaleValue(1, 1)}})
 }
