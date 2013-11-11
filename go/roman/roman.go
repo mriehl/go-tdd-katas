5dd518331fe38c291a213d743806bdf058e397e9
mriehl <maximilien.riehl@gmail.com>
2013-11-11 13:51:19 +0100

    type name can be omitted in literal's elements

--- go/roman/roman.go
+++ go/roman/roman.go
@@ -14,19 +14,19 @@ func DivMod(number uint16, divisor uint16) (quotient, remainder uint16) {
 func AsRoman(decimal uint16) (roman string) {
 
 	var mapping = []RomanAtom{
-		RomanAtom{DecimalValue: 1000, Rune: "M"},
-		RomanAtom{DecimalValue: 900, Rune: "CM"},
-		RomanAtom{DecimalValue: 500, Rune: "D"},
-		RomanAtom{DecimalValue: 400, Rune: "CD"},
-		RomanAtom{DecimalValue: 100, Rune: "C"},
-		RomanAtom{DecimalValue: 90, Rune: "XC"},
-		RomanAtom{DecimalValue: 50, Rune: "L"},
-		RomanAtom{DecimalValue: 40, Rune: "XL"},
-		RomanAtom{DecimalValue: 10, Rune: "X"},
-		RomanAtom{DecimalValue: 9, Rune: "IX"},
-		RomanAtom{DecimalValue: 5, Rune: "V"},
-		RomanAtom{DecimalValue: 4, Rune: "IV"},
-		RomanAtom{DecimalValue: 1, Rune: "I"},
+		{DecimalValue: 1000, Rune: "M"},
+		{DecimalValue: 900, Rune: "CM"},
+		{DecimalValue: 500, Rune: "D"},
+		{DecimalValue: 400, Rune: "CD"},
+		{DecimalValue: 100, Rune: "C"},
+		{DecimalValue: 90, Rune: "XC"},
+		{DecimalValue: 50, Rune: "L"},
+		{DecimalValue: 40, Rune: "XL"},
+		{DecimalValue: 10, Rune: "X"},
+		{DecimalValue: 9, Rune: "IX"},
+		{DecimalValue: 5, Rune: "V"},
+		{DecimalValue: 4, Rune: "IV"},
+		{DecimalValue: 1, Rune: "I"},
 	}
 
 	for _, romanAtom := range mapping {
