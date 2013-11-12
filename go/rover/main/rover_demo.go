e6eb23d8054b8107d7e5e0206a0dcffc95c2a320
mriehl <maximilien.riehl@gmail.com>
2013-11-12 11:47:34 +0100

    rover usage example

---
+++ go/rover/main/rover_demo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/mriehl/katas/go/rover"
+	"log"
+	"os"
+)
+
+func main() {
+	logger := log.New(os.Stderr, "[RoverDemo] ", log.Flags())
+	r := rover.New(rover.Coordinates{99, 99}, rover.North)
+	r.Grid.Insert(rover.Coordinates{99, 1}, rover.OBSTACLE)
+
+	remote := rover.NewRemoteControl(r)
+	remote.Send("F", "F")
+	logger.Println("Rover is now at ", r.Coords)
+}
